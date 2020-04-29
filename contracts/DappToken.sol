6bd3bb7f5db46f365bf6d3f485ee968bb14b74ed
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-04-29 18:10:11 -0300

    Approve function, e evento criados

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -10,7 +10,14 @@ contract DappToken {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf; //returns the account balance of another account with address _owner.
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         //msg.sender is the initial account
@@ -32,6 +39,17 @@ contract DappToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value; //allowing transfer
+
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
